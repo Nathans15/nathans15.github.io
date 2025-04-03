c5fc98a07aa879b6d70bc44282f7d80276f4c90c
Nathan Sheldrake <nathansheldrake151@gmail.com>
2025-04-03 19:44:24 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,21 +5,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilename = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imageFilename = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const imageAltText = [{altText1 : "eye"},
-                      {altText2 : "seashell"},
-                      {altText3 : "flowers"},
-                      {altText4 : "painting"},
-                      {altText5 : "butterfly"}];
+const imageAltText = ["human eye", "seashell", "flowers", "painting", "butterfly"];
 
 /* Looping through images */
 for (let i = 0; i < imageFilename.length; i++) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/' + imageFilename[i]);
+    newImage.setAttribute('src', imageFilename[i]);
     newImage.setAttribute('alt', imageAltText[i]);
     thumbBar.appendChild(newImage);
 }
+
+newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', newImage.getAttribute('src'));
+    displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+});
 
 /* Wiring up the Darken/Lighten button */
