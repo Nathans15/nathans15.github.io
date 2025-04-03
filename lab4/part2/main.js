1585b31e3cb8391040908dd1a03b736b1abbb2c2
Nathan Sheldrake <nathansheldrake151@gmail.com>
2025-04-03 17:15:36 -0400

    added image filenames and alt text

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,8 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilename = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imageAltText = [{altText1 : "eye"},
+                      {altText2 : "seashell"},
+                      {altText3 : "flowers"},
+                      {altText4 : "painting"},
+                      {altText5 : "butterfly"}];
 
 /* Looping through images */
 
