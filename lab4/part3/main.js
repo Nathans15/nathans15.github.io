fa967e4b0ecb39a3160ee312cb87bb8075f03ddc
Nathan Sheldrake <nathansheldrake151@gmail.com>
2025-04-04 14:47:04 -0400

    Update main.js

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -46,7 +46,20 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
       }
+
+      collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
       
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
       
     constructor(x, y, velX, velY, color, size) {
       this.x = x;
@@ -58,6 +71,7 @@ class Ball {
     }
   }
 
+  
 const balls = [];
 
 while (balls.length < 25) {
@@ -88,5 +102,6 @@ function loop() {
     requestAnimationFrame(loop);
     }
   
+loop();
 
 
